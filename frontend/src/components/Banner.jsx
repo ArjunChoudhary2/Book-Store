7ba750c56d9e7dc57f2700ebf5fb54bb9ba0fcfd
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-12-20 09:31:01 +0530

    Nav bar implemented

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const Banner = () => {
+  return (
+    <div className="px-4 lg:px-24 bg-teal-100 flex items-center">
+      <div className=" flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40">
+        <div className="md:w-1/2 space-y-7 h-full">
+          <h2 className="text-3xl font-bold leading-snug text-black">
+            Buy and Sell Your Books
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search a Book"
+            className="py-2 px-2 rounded-s-sm outline-border-none"
+          ></input>
+          <button className="bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200">
+            Search
+          </button>
+        </div>
+        <div>Right Side</div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBarsStaggered, FaBlog } from 'react-icons/fa6';
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6";
 
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
+
+  //toggle Menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,27 +38,68 @@ const Nav = () => {
   ];
 
   return (
-    <header>
-      <nav>
-        <div>
+    <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
+      <nav
+        className={`py-4 lg:px-24 px-4 ${
+          isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
           {/* Logo */}
-          <Link to="/" className="text-2xl font-bold text-blue-700 flex items-center gap-2">
-            <FaBlog className="inline-block"/> Books
+          <Link
+            to="/"
+            className="text-2xl font-bold text-blue-700 flex items-center gap-2"
+          >
+            <FaBlog className="inline-block" /> Books
           </Link>
-        </div>
-        {/* nav items for large device */}
-        <ul className="md:flex space-x-12 hidden ">
-          {
-            navItems.map(({ link, path }) => (
+          {/* nav items for large device */}
+          <ul className="md:flex space-x-12 hidden ">
+            {navItems.map(({ link, path }) => (
               <li key={path}>
-                <Link to={path} className="block text-base text-black uppercase cursor-pointer hover:text-blue-700">
+                <Link
+                  to={path}
+                  className="block text-base text-black uppercase cursor-pointer hover:text-blue-700"
+                >
                   {link}
                 </Link>
               </li>
-            ))
-          }
-        </ul>
-        <div className="space-x-12 hidden lg:flex item-center"><button><FaBarsStaggered className="w-5 hover:text-blue-700"/></button></div>
+            ))}
+          </ul>
+          <div className="space-x-12 hidden lg:flex item-center">
+            <button>
+              <FaBarsStaggered className="w-5 hover:text-blue-700" />
+            </button>
+          </div>
+        </div>
+
+        {/* menu btn for mobile devices */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            {isMenuOpen ? (
+              <FaXmark className="h-5 w-5 text-black" />
+            ) : (
+              <FaBarsStaggered className="h-5 w-5 text-black" />
+            )}
+          </button>
+
+          {/* nav items for sm devices */}
+          <div
+            className={`space-y-4 px-4 mt-16 py-7 bg-blue-700 ${
+              isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+            }`}
+          >
+            {navItems.map(({ link, path }) => (
+              <li key={path}>
+                <Link
+                  to={path}
+                  className="block text-base text-white uppercase cursor-pointer"
+                >
+                  {link}
+                </Link>
+              </li>
+            ))}
+          </div>
+        </div>
       </nav>
     </header>
   );
