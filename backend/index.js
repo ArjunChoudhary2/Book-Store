613259b94f2be3c6cab12c9e3d82a23c06a52203
Arjun Choudhary <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-12-27 12:56:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  "mongodb+srv://bookstore_user:bookstore1user@book-store-mern.bhubzxl.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://bookstore_user:######@book-store-mern.bhubzxl.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
